d5ca733d14f154dc4812ebe8c3077c3216d9ba22
Henri Ihalainen <henri.ihalainen@gofore.com>
2023-11-28 13:45:49 +0200

    Add kWh to charging power

--- src/charging-api.ts
+++ src/charging-api.ts
@@ -1,30 +1,6 @@
-import { ChargingStation } from './charging-station';
-
 export interface ChargingStatus {
     chargedCapacityKwH: number;
     capacityPercentage: number;
     charging: boolean;
-    chargingPower: number;
-}
-
-export class ChargingApi {
-    constructor(private chargingStation: ChargingStation) {}
-
-    startCharging() {
-        this.chargingStation.startCharging();
-    }
-
-    stopCharging() {
-        this.chargingStation.stopCharging();
-    }
-
-    getChargingStatus(): ChargingStatus {
-        return {
-            chargedCapacityKwH: this.chargingStation.chargedCapacityKwH,
-            capacityPercentage: this.chargingStation.capacityPercentage,
-            charging: this.chargingStation.charging,
-            chargingPower: this.chargingStation.chargingPower,
-        };
-    }
-
+    chargingPowerKwH: number;
 }
